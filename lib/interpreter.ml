e65179b4a97455e1924e612106ec293e4bed7549
KirrimK <KirrimK@users.noreply.github.com>
2024-12-06 12:19:38 +0100

    Add expression evaluation

--- lib/interpreter.ml
+++ lib/interpreter.ml
@@ -23,6 +23,8 @@ let init_memory (): memory =
 
 let rec run_line (memory: memory) (code: code) (ip:int) : (memory * int) =
   let (line_array, line_assoc) = code in
+  if ip >= (Array.length line_array) then
+    raise Halted;
   let statement = match line_array.(ip) with
                     NumberedStatement (_, s) -> s
                   | Statement s -> s in
@@ -30,43 +32,81 @@ let rec run_line (memory: memory) (code: code) (ip:int) : (memory * int) =
 
 and run_statement (memory: memory) (line_assoc: (int * int) list) (ip: int) (statement: statement): memory * int =
   let (var_assoc, ret_stack) = memory in
-  match statement with
-    Print el ->
-      print_endline (String.concat "" (List.map value_to_string (List.map (fun x -> calc_expr memory x) el))); (memory, ip+1)
-  | If (ea, rl, eb, stat) ->
-      if calc_comp memory ea rl eb then
-        run_statement memory line_assoc ip stat
-      else
-        (memory, ip+1)
-  | Goto e -> begin match calc_expr memory e with
-                Number n -> (memory, List.assoc n line_assoc)
-              | String _ -> failwith "Cannot jump to non-int line number" end
-  | Input _ -> raise Not_implemented
-  | Let (s, e) -> let v = calc_expr memory e in (((s, v)::var_assoc, ret_stack), ip+1)
-  | Gosub e -> begin match calc_expr memory e with
-                 Number n -> ((var_assoc, (ip+1)::ret_stack), List.assoc n line_assoc)
-               | String _ -> failwith "Cannot jump to non-int line number" end
-  | Return ->  ((var_assoc, List.tl ret_stack), List.hd ret_stack)
-  | Clear -> failwith "Clear command for interactive prompt only"
-  | List -> failwith "List command for interactive prompt only"
-  | Run -> failwith "Run command for interactive prompt only"
-  | End -> raise Halted
+  try
+    begin match statement with
+      Print el ->
+        print_endline (String.concat "" (List.map value_to_string (List.map (fun x -> calc_expr memory x) el))); (memory, ip+1)
+    | If (ea, rl, eb, stat) ->
+        if calc_comp memory ea rl eb then
+          run_statement memory line_assoc ip stat
+        else
+          (memory, ip+1)
+    | Goto e -> begin match calc_expr memory e with
+                  Number n -> (memory, List.assoc n line_assoc)
+                | String _ -> failwith "Cannot jump to non-int line number" end
+    | Input _ -> raise Not_implemented
+    | Let (s, e) -> let v = calc_expr memory e in (((s, v)::var_assoc, ret_stack), ip+1)
+    | Gosub e -> begin match calc_expr memory e with
+                  Number n -> ((var_assoc, (ip+1)::ret_stack), List.assoc n line_assoc)
+                | String _ -> failwith "Cannot jump to non-int line number" end
+    | Return ->  ((var_assoc, List.tl ret_stack), List.hd ret_stack)
+    | Clear -> failwith "Clear command for interactive prompt only"
+    | List -> failwith "List command for interactive prompt only"
+    | Run -> failwith "Run command for interactive prompt only"
+    | End -> raise Halted end
+  with
+    Failure s -> failwith ("Error on line " ^ (string_of_int ip) ^ " (" ^ (statement_to_string statement) ^ "): " ^ s)
 
 and calc_expr (memory: memory) (e: expression) : value =
   let (var_assoc, _) = memory in
   match e with
   | Value v -> v
-  | Var name -> List.assoc name var_assoc
-  | _ -> raise Not_implemented
+  | Var name -> 
+      begin try
+        List.assoc name var_assoc
+      with Not_found -> failwith ("Variable '" ^ name ^ "' not found") end
+  | Plus (ea, eb) -> 
+      begin match calc_expr memory ea, calc_expr memory eb with
+        Number na, Number nb -> Number (na+nb)
+      | String sa, String sb -> String (sa^sb)
+      | _ -> failwith "Unsupported operation" end
+  | Minus (ea, eb) -> 
+      begin match calc_expr memory ea, calc_expr memory eb with
+        Number na, Number nb -> Number (na-nb)
+      | _ -> failwith "Unsupported operation" end
+  | Mult (ea, eb) ->
+      begin match calc_expr memory ea, calc_expr memory eb with
+        Number na, Number nb -> Number (na*nb)
+      | _ -> failwith "Unsupported operation" end
+  | Div (ea, eb) ->
+      begin match calc_expr memory ea, calc_expr memory eb with
+        Number na, Number nb -> Number (na/nb)
+      | _ -> failwith "Unsupported operation" end
+  | UnaryMinus (e) ->
+      begin match calc_expr memory e with
+        Number n -> Number (-n)
+      | String _ -> failwith "Unsupported operation" end
 
 and calc_comp (memory: memory) ea rl eb: bool =
   let va = calc_expr memory ea in
   let vb = calc_expr memory eb in
   match rl with
-    Lt ->  raise Not_implemented
-  | Lte -> raise Not_implemented
-  | Gt -> raise Not_implemented
-  | Gte -> raise Not_implemented
+    Lt -> 
+      begin match va, vb with
+        Number na, Number nb -> na < nb
+      | _, _ -> failwith "Unsupported operation" end
+  | Lte -> 
+      begin match va, vb with
+        Number na, Number nb -> na <= nb
+      | _, _ -> failwith "Unsupported operation" end
+  | Gt -> 
+      begin match va, vb with
+        Number na, Number nb -> na > nb
+      | _, _ -> failwith "Unsupported operation" end
+  | Gte ->
+      begin match va, vb with
+        Number na, Number nb -> na >= nb
+      | _, _ -> failwith "Unsupported operation" end
   | Eq -> va = vb
   | Diff -> va <> vb
   | Frown -> raise Not_implemented
