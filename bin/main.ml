4c71e29f3570236296b0dac116507c7923e77f14
KirrimK <KirrimK@users.noreply.github.com>
2024-12-06 12:19:18 +0100

    Add interpreter error handling

--- bin/main.ml
+++ bin/main.ml
@@ -18,23 +18,30 @@ let done_ = ref true;;
 while !done_ do
 
   print_string "> ";
-  let temp_input = (read_line ()) in
+  let temp_input = 
+    try
+      read_line ()
+    with
+      End_of_file -> done_:=false; print_endline ""; "" in
   if temp_input = "run" then
-    let lexbuf = Lexing.from_string !input in
-    let line_list = Parser.line_list Lexer.token lexbuf in
-    let code = line_list_to_code line_list in 
-    run_lines memory code;
+    try 
+      let lexbuf = Lexing.from_string !input in
+      let line_list = Parser.line_list Lexer.token lexbuf in
+      let code = line_list_to_code line_list in 
+      run_lines memory code
+    with
+      Halted -> ()
+    | Lexer.Error s -> print_endline s
+    | Parser.Error -> print_endline "Syntax error"
+    | Failure s -> print_endline s
   else if temp_input = "quit" then
     done_:=false
   else if temp_input = "clear" then
     input:=""  
   else if temp_input = "list" then
-    print_endline !input
+    print_string !input
   else
-    if !input = "" then
-      input:=temp_input
-    else
-      input:=!input ^ "\n" ^ temp_input
+    input:=!input ^ temp_input ^ "\n" 
   
 
 done
